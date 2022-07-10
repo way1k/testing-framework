801dc632fb6487ac5c2563dacef4f55daf08f9d4
Vlad Sokolov <sokolov.v.e@effective-group.ru>
2022-07-11 00:01:15 +0300

    Updated base_page.py Added pre-commit and config files Changed README.md Added new libraries in requirements.txt Changed host for Allure-server Translate to eng all comments and descriptions Full refactoring framewor Added pydantic models Added methods Added endpoints Added integration with allure docker ui Added provide run property etc

--- tests/bashorg/api/test_bashorg_failed.py
+++ tests/bashorg/api/test_bashorg_failed.py
@@ -3,7 +3,7 @@ import allure
 
 @allure.title("Specially falling test")
 @allure.label("platform", "Bashorg")
-def test_bashorg_failed(platform):
+def test_bashorg_failed():
 
     with allure.step("Open the main page"):
         assert 1 == 0
--- tests/bashorg/api/test_bashorg_passed.py
+++ tests/bashorg/api/test_bashorg_passed.py
@@ -3,7 +3,7 @@ import allure
 
 @allure.title("Specially passing test")
 @allure.label("platform", "Bashorg")
-def test_bashorg_passed(platform):
+def test_bashorg_passed():
 
     with allure.step("Open the home page"):
         assert 1 == 1
